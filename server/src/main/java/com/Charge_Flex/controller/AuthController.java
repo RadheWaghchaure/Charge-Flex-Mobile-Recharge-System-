5f846f63fb93f1bb8fe33394c14fa8865cc98c99
Radhesham <radheshamwaghchaure@gmail.com>
2025-02-08 21:52:19 +0530

    Created Trasaction table and api for payment gateway

--- server/src/main/java/com/Charge_Flex/controller/AuthController.java
+++ server/src/main/java/com/Charge_Flex/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -124,8 +125,9 @@ public class AuthController {
         
     }
 
-	@GetMapping("/createTransaction/{amount}")
-	public TransactionDetails createTransaction(@PathVariable(name="") Double amount) {
+	@PostMapping("/createTransaction/{amount}")
+	@CrossOrigin("*")
+	public TransactionDetails createTransaction(@PathVariable("amount") Double amount) {
 		return authService.createTransaction(amount);
 	}
 	
